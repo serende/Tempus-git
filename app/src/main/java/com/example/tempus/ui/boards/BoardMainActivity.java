dad275b39bda21b35dd584f713b26906170ee1a8
serende <55824287+serende@users.noreply.github.com>
2021-02-25 14:55:42 +0900

    osr basic setting of writing view

---
+++ app/src/main/java/com/example/tempus/ui/boards/BoardMainActivity.java
@@ -0,0 +1,72 @@
+package com.example.tempus.ui.boards;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.tempus.ui.addSchedule.expenditureBreakdownActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.tempus.R;
+
+public class BoardMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("게시판");
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 게시판 추가 페이지로 이동하도록 변경해야 함
+                Intent intent = new Intent(getApplicationContext(), AddBoardActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.boardmenu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.action_search:
+                Toast.makeText(getApplicationContext(), "검색",
+                        Toast.LENGTH_SHORT).show();
+            case R.id.changeDisplayCalendar:
+                Toast.makeText(getApplicationContext(), "캘린더형으로 변경",
+                        Toast.LENGTH_SHORT).show();
+            case R.id.changeDisplaySlide:
+                Toast.makeText(getApplicationContext(), "슬라이드형으로 변경",
+                        Toast.LENGTH_SHORT).show();
+            case R.id.registerFriends:
+                Toast.makeText(getApplicationContext(), "지인 등록",
+                        Toast.LENGTH_SHORT).show();
+            case R.id.friendsList:
+                Toast.makeText(getApplicationContext(), "지인 목록 보기",
+                        Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/tempus/ui/boards/ExpenditureBreakdownActivityForWrite.java
@@ -0,0 +1,46 @@
+package com.example.tempus.ui.boards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.tempus.R;
+
+public class ExpenditureBreakdownActivityForWrite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenditure_breakdown);
+
+        TextView dayView = (TextView) findViewById(R.id.dayView);
+
+        Button changeDisplay = (Button) findViewById(R.id.changeDisplay);
+        changeDisplay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), WriteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button finButton = (Button) findViewById(R.id.finButton);
+        finButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                // TODO
+            }
+        });
+
+        EditText productNameEdit = (EditText) findViewById(R.id.productNameEdit);
+        EditText priceEdit = (EditText) findViewById(R.id.priceEdit);
+        EditText purchaseDateEdit = (EditText) findViewById(R.id.purchaseDateEdit);
+        EditText tagEdit = (EditText) findViewById(R.id.tagEdit);
+        EditText memoEdit = (EditText) findViewById(R.id.memoEdit);
+    }
+}
