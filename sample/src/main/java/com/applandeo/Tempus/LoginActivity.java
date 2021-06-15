303de3e5d6a074b7d040724f4c411d2c38ab362d
rlaalsrl <real1245@gmail.com>
2021-06-15 17:12:01 +0900

    6/15 rlaalsrl 회원가입 액티비티 개발 진행중

--- sample/src/main/java/com/applandeo/Tempus/LoginActivity.java
+++ sample/src/main/java/com/applandeo/Tempus/LoginActivity.java
@@ -100,7 +100,7 @@ public class LoginActivity extends Activity {
             //String hjson = params[0];
             String userdata = params[0];
             try {
-                String host_url = "http://192.168.0.3:5000/login";
+                String host_url = "http://192.168.60.149:5000/login";
                 URL url = new URL(host_url);
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setConnectTimeout(15*1000);//Timeout setting
--- sample/src/main/java/com/applandeo/Tempus/SignupActivity.java
+++ sample/src/main/java/com/applandeo/Tempus/SignupActivity.java
@@ -1,14 +1,46 @@
 package com.applandeo.Tempus;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
-
+    private EditText mNameView;
+    private EditText mPhoneNumberView;
+    private EditText mAddressView;
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private EditText mPassword2View;
+    String name,pnum,address,email,password,password2;
+    String userjson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mNameView = (EditText) findViewById(R.id.signup_name);
+        mPhoneNumberView = (EditText) findViewById(R.id.signup_phonenumber);
+        mAddressView = (EditText) findViewById(R.id.signup_office);
+        mEmailView = (EditText) findViewById(R.id.signup_email);
+        mPasswordView = (EditText) findViewById(R.id.signup_password);
+        mPassword2View = (EditText) findViewById(R.id.signup_password2);
+
+        Button SignupButton = (Button) findViewById(R.id.signup_button); // signup button
+        SignupButton.setOnClickListener(v ->{
+            name = mNameView.getText().toString();
+            pnum = mPhoneNumberView.getText().toString();
+            address = mAddressView.getText().toString();
+            email = mEmailView.getText().toString();
+            password = mPasswordView.getText().toString();
+            password2 = mPassword2View.getText().toString();
+
+            
+
+
+
+        });
+
     }
 }
