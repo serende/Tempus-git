81e947d6dee441b351eb247b3be45dee72a46db7
serende <55824287+serende@users.noreply.github.com>
2021-08-16 13:55:04 +0900

    8/16 osr

--- sample/src/main/java/com/example/tempus/ui/boards/BoardMainActivity.java
+++ sample/src/main/java/com/example/tempus/ui/boards/BoardMainActivity.java
@@ -30,6 +30,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.Toast;
 import android.widget.Button;
@@ -37,6 +39,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class BoardMainActivity extends AppCompatActivity {
+
+    private Animation fab_open, fab_close;
+    private Boolean isFabOpen = false;
+
+    private FloatingActionButton openFAB, addBoardFAB, friendFAB, shoppingFAB;
+    private ImageButton imageButton1, imageButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,38 +55,87 @@ public class BoardMainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("게시판");
 
+        FloatingActionButton openFAB = findViewById(R.id.openFAB);
+        openFAB.setOnClickListener(v -> {
+            anim();
+        });
+
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+
+        try{
+            addBoardFAB = findViewById(R.id.addBoardFAB);
+            friendFAB = findViewById(R.id.friendFAB);
+            shoppingFAB = findViewById(R.id.shoppingFAB);
+        } catch (Exception e){
+            Log.e("FVBERROR", e.toString());
+        }
+
         // 게시판 추가 액티비티로 이동
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> {
+        addBoardFAB.setOnClickListener(v -> {
+            anim();
             Intent intent = new Intent(getApplicationContext(), AddBoardActivity.class);
             startActivity(intent);
         });
 
         // 지인 목록 액티비티로 이동
-        FloatingActionButton friendFAB = findViewById(R.id.friendFAB);
         friendFAB.setOnClickListener(v -> {
+            anim();
             Intent BMAIntent = new Intent(getApplicationContext(), FriendListActivity.class);
             startActivity(BMAIntent);
         });
 
-        FloatingActionButton shoppingFAB = findViewById(R.id.shoppingFAB);
         shoppingFAB.setOnClickListener(v -> {
+            anim();
             // TODO
         });
 
-        ImageButton imageButton1 = findViewById(R.id.imageButton1);
+        imageButton1 = findViewById(R.id.imageButton1);
         imageButton1.setOnClickListener(view -> {
             Intent baIntent = new Intent(getApplicationContext(), boardActivity.class);
             baIntent.putExtra("그룹명", "2팀 게시판");
             startActivity(baIntent);
         });
 
-        ImageButton imageButton2 = findViewById(R.id.imageButton2);
+        imageButton2 = findViewById(R.id.imageButton2);
         imageButton2.setOnClickListener(view -> {
             Intent baIntent2 = new Intent(getApplicationContext(), boardActivity.class);
             baIntent2.putExtra("그룹명", "우리 가족방");
             startActivity(baIntent2);
         });
+    }
+
+    public void anim() {
+        if(isFabOpen) {
+            try{
+                addBoardFAB.startAnimation(fab_close);
+                friendFAB.startAnimation(fab_close);
+                shoppingFAB.startAnimation(fab_close);
+
+                addBoardFAB.setClickable(false);
+                friendFAB.setClickable(false);
+                shoppingFAB.setClickable(false);
+
+                isFabOpen = false;
+            } catch (Exception e){
+                Log.e("animERROR", e.toString());
+            }
+
+        } else {
+            try{
+                addBoardFAB.startAnimation(fab_open);
+                friendFAB.startAnimation(fab_open);
+                shoppingFAB.startAnimation(fab_open);
+
+                addBoardFAB.setClickable(true);
+                friendFAB.setClickable(true);
+                shoppingFAB.setClickable(true);
+
+                isFabOpen = true;
+            } catch (Exception e){
+                Log.e("animERROR", e.toString());
+            }
+        }
     }
 
     /*
